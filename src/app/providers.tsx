e144e4de3e4aefd6c2108e7d576f603d360d0c37
Irvin Elbanbuena <irvin@ingenuity.ph>
2025-04-13 23:14:29 +0800

    feat: implement React Query provider and update layout structure

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { ReactNode } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
