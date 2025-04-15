11d57280bdb508371bf094656a5ebb6f75580e08
Irvin Elbanbuena <irvin@ingenuity.ph>
2025-04-16 03:35:24 +0800

    feat: add pagination support and utility functions for restaurant search

--- src/features/restaurant-finder/lib/generateRestaurantSearchQuery.ts
+++ src/features/restaurant-finder/lib/generateRestaurantSearchQuery.ts
@@ -1,18 +1,34 @@
-import { UserMessage } from "@/features/restaurant-finder/schema/userMessage";
+import { UserMessage } from '@/features/restaurant-finder/schema/userMessage'
+import { PageParam } from '@/features/restaurant-finder/schema/pagination'
+interface GenerateRestaurantSearchQueryProps {
+  data: UserMessage
+  pageParam: PageParam
+}
 
-export const generateRestaurantSearchQuery = async (data: UserMessage) => {
-  const response = await fetch("/api/execute", {
-    method: "POST",
+export const generateRestaurantSearchQuery = async ({
+  data,
+  pageParam,
+}: GenerateRestaurantSearchQueryProps) => {
+  const response = await fetch('/api/execute', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
-  });
+    body: JSON.stringify({
+      ...data,
+      cursor: pageParam?.nextCursor ?? null,
+      params: pageParam?.params ?? null,
+    }),
+  })
 
   if (!response.ok) {
-    throw new Error("Network response was not ok");
+    throw new Error('Network response was not ok')
   }
-  const responseData = await response.json();
-  console.log(responseData);
-  return responseData;
-};
+  const responseData = await response.json()
+
+  return {
+    results: responseData.results,
+    nextCursor: responseData.nextCursor,
+    params: responseData.params,
+  }
+}
